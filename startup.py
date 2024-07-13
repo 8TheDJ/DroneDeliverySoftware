f2ed0200dc0fba8a778c1fe8dca65bad1a9d82ac
8TheDJ <diederik250@gmail.com>
2024-07-13 11:25:07 +0200

    Create startup.py

---
+++ startup.py
@@ -0,0 +1,7 @@
+from dronekit import *
+import time
+import socket
+import argparse
+import math
+
+#Doel om het starten van de drone te testen, dus hij moet gewoon zijn rotoren aanzetten en weer loslaten, zodat we kleine modules bouwen die je bij mekaar kan doen
